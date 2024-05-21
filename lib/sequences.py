c1744d8caad3790efe082dbfee169d5e132236fc
whitney <shisiawhitney215@gmail.com>
2024-05-21 16:11:17 +0300

    py

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    
+    fibonacci_sequence = []
+    a, b = 0, 1
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+    
+    print(fibonacci_sequence)
